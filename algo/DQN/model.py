7c191509b07f6e15f226a84ef34b755babb8d792
kibeom <ki.kim@jayecorp.com>
2021-12-18 16:58:36 +0900

    modified DQN

--- algo/DQN/model.py
+++ algo/DQN/model.py
@@ -49,27 +49,43 @@ class DQN(nn.Module):
         if self.buffer.__len__() < self.batch_size:
             return
 
+        # sample transition from experience memory.
         transitions = self.buffer.sample(self.batch_size)
         batch = Transition(*zip(*transitions))
 
-        # calculate return of all times in the episode
-        reward_list = self.buffer.rewards
-        T = len(reward_list)
-        returns = np.empty(T, dtype=np.float32)
-        future_return = 0.0
+        # Compute a mask of non-final states and concatenate the batch elements
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
 
-        # calculate returns recursively
-        for t in reversed(range(T)):
-            future_return = reward_list[t] + self.gamma * future_return
-            returns[t] = future_return
+        # tensor-ize batch samples.
+        state_batch  = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
 
-        # calculate loss of policy
-        returns = torch.tensor(returns)
-        log_probs = torch.stack(self.buffer.logprobs)
-        loss = - log_probs * returns
-        loss = torch.sum(loss)
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.QNet(state_batch).gather(1, action_batch)
 
-        # do gradient ascent
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.batch_size)
+        next_state_values[non_final_mask] = self.QTarget(non_final_next_states).max(1)[0].detach()
+
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
         self.optimizer.zero_grad()
         loss.backward()
+
+        for param in self.QNet.parameters():
+            param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
